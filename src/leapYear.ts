dbea9665215828865f3a1e58302cb0d3b5906057
matt emerson <matthewwemerson@gmail.com>
2020-08-14 16:58:06 +0100

    test 2/3

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,3 +1,9 @@
-export const leapYear = (inputYear: number): boolean => {
-  return inputYear === 1996;
+const isleapYear = (inputYear: number): boolean => {
+  return isMultipleOf4(inputYear);
 };
+
+const isMultipleOf4 = (inputYear: number): boolean => {
+  return inputYear % 4 === 0;
+};
+
+export default isleapYear;
--- tests/leapYear.test.ts
+++ tests/leapYear.test.ts
@@ -1,7 +1,19 @@
-import { leapYear } from "../src/leapYear";
+import isleapYear from "../src/leapYear";
 
 describe("When I check if 1996 is a leap year", () => {
   it("should return 'true'", () => {
-    expect(leapYear(1996)).toBe(true);
+    expect(isleapYear(1996)).toBe(true);
+  });
+});
+
+describe("When I check if 2001 is a leap year", () => {
+  it("should return 'false'", () => {
+    expect(isleapYear(2001)).toBe(false);
+  });
+});
+
+describe("When I check if 2000 is a leap year", () => {
+  it("should return 'true'", () => {
+    expect(isleapYear(2000)).toBe(true);
   });
 });
