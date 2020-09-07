985e594e4f036708ce97442989da67b3eb717606
matt emerson <matthewwemerson@gmail.com>
2020-09-07 09:51:48 +0100

    addressed PR issues

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,13 +1,13 @@
-const leapYear = (inputYear: number): boolean => {
-  return isTypicalLeapYear(inputYear) || isAtypicalLeapYear(inputYear);
+const isLeapYear = (inputYear: number): boolean => {
+  return isTypicalLeapYear(inputYear) || isNonTypicalLeapYear(inputYear);
 };
 
 const isTypicalLeapYear = (inputYear: number): boolean => {
   return inputYear % 4 === 0 && inputYear % 100 !== 0;
 };
 
-const isAtypicalLeapYear = (inputYear: number): boolean => {
-  return inputYear % 4 === 0 && inputYear % 100 === 0 && inputYear % 400 === 0;
+const isNonTypicalLeapYear = (inputYear: number): boolean => {
+  return inputYear % 400 === 0;
 };
 
-export default leapYear;
+export default isLeapYear;
