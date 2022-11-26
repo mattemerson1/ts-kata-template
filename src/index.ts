3c1b13c3dc94227470ab8af3a09a901b752b2fb8
Matt Emerson <matthew.emerson@ibm.com>
2022-11-26 19:46:39 +0000

    booleanCalculator complete

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+export const booleanCalculator = (input:string):boolean | void => {
+    let evaluatedStatement = [];
+    input = formatInput(input);
+    
+    let inputArray: string[] = input.split(" ");
+    evaluatedStatement = evaluator(inputArray);
+    if (input == "NOT TRUE" || input == "FALSE") {
+        return false;
+    } else if (input == "NOT FALSE" || input == "TRUE") {
+        return true;
+    } else if (evaluatedStatement[0] == "TRUE") {
+        return true;
+    } else if (evaluatedStatement[0] == "FALSE") {
+        return false;
+    }
+}
+
+const formatInput = (input:string):string => {
+    let formattedInput:string[] = Array.from(input);
+    for (let i=0; i<formattedInput.length; i++) {
+        if (formattedInput[i] == '(') {
+          formattedInput.splice(i + 1,0, ' ');
+          break;
+        }
+    }
+    for (let j=0; j<formattedInput.length; j++) {
+        if (formattedInput[j] == ')') {
+          formattedInput.splice(j,0,' ');
+          break;
+        }
+    }
+    return formattedInput.join('');
+
+}
+
+const evaluator = (statementArray:string[]):string[] => {
+    let parenthesesEvaluated = evaluateParentheses(statementArray)
+    let notEvalulated = evaluateNot(parenthesesEvaluated);
+    let andEvaluated = evaluateAnd(notEvalulated);
+    let orEvaluated = evaluateOr(andEvaluated);
+    if (statementArray.length > 1) {
+        evaluator(orEvaluated);
+    }
+    return orEvaluated;
+}
+
+const evaluateParentheses = (statementArray:string[]):string[] => {
+    let firstParentheses:number = 0;
+    let secondParentheses:number = 0;
+    for (let i=0;i<statementArray.length;i++) {
+        if(statementArray[i] == "(") {
+          firstParentheses = i;
+        } else if (statementArray[i] == ')') {
+          secondParentheses = i;
+          let innerStatement = evaluator(statementArray.slice(firstParentheses + 1,secondParentheses));
+          statementArray.splice(firstParentheses, (secondParentheses - firstParentheses) + 1, innerStatement[0])
+        }
+    }
+    return statementArray;
+}
+
+const evaluateNot = (statementArray:string[]):string[] => {
+  for (let i=0;i<statementArray.length;i++) {
+    if (statementArray[i] == "NOT" && statementArray[i + 1] == "TRUE") {
+        statementArray[i] = "FALSE";
+        statementArray.splice(i + 1,1);
+    } else if (statementArray[i] == "NOT" && statementArray[i + 1] == "FALSE") {
+        statementArray[i] = "TRUE";
+        statementArray.splice(i + 1,1);
+    }
+  }
+  return statementArray;
+};
+
+const evaluateAnd = (statementArray:string[]):string[] => {
+  for (let i=0;i<statementArray.length;i++) {
+    if (statementArray[i] == "AND" && (statementArray[i - 1] == "TRUE" && statementArray[i + 1] == "TRUE")) {
+      statementArray[i] = "TRUE";
+      statementArray.splice(i - 1,1);
+      statementArray.splice(i,1);
+    } else if (statementArray[i] == "AND" && (statementArray[i - 1] == "FALSE" || statementArray[i + 1] == "FALSE")) {
+      statementArray[i] = "FALSE";
+      statementArray.splice(i - 1,1);
+      statementArray.splice(i,1);
+    };
+  };
+  return statementArray;
+}
+const evaluateOr = (statementArray:string[]):string[] => {
+  for (let i=0;i<statementArray.length;i++) {
+    if (statementArray[i] == 'OR' && (statementArray[i - 1] == "TRUE" || statementArray[i + 1] == "TRUE")) {
+      statementArray[i] = "TRUE";
+      statementArray.splice(i - 1,1);
+      statementArray.splice(i,1);
+    } else if (statementArray[i] == 'OR' && (statementArray[i - 1] == "FALSE" && statementArray[i + 1] == "FALSE")) {
+      statementArray[i] = "FALSE";
+      statementArray.splice(i - 1,1);
+      statementArray.splice(i,1);
+    };
+  };
+  return statementArray;
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,52 +1,64 @@
-import { findPrimeFactorials } from "../src/primeFactorials"
+import { booleanCalculator } from "../src";
 
-describe("return correct prime factor for single digit prime number", () => {
-  it.each([
-    [2, [2]],
-    [3, [3]],
-    [5, [5]],
-    [7, [7]],
-  ])(`should take a number and return an array of its prime factorials`, (a, expected) => {
-
-    expect(findPrimeFactorials(a)).toStrictEqual(expected);
-  });
-});
-
-describe("return correct prime factor for single digit number", () => {
-  it.each([
-    [4, [2, 2]],
-    [6, [2, 3]],
-    [8, [2, 2, 2]],
-    [9, [3, 3]]
-  ])(`should take a number and return an array of its prime factorials`, (a, expected) => {
-
-    expect(findPrimeFactorials(a)).toStrictEqual(expected);
-  });
-});
-
-describe("return correct prime factor for double digit prime numbers", () => {
-  it.each([
-    [11, [11]],
-    [19, [19]],
-    [23, [23]],
-    [61, [61]],
-    [67, [67]],
-    [79, [79]]
-  ])(`should take a number and return an array of its prime factorials`, (a, expected) => {
-
-    expect(findPrimeFactorials(a)).toStrictEqual(expected);
-  });
-});
-
-describe("return correct prime factor for double digit numbers", () => {
-  it.each([
-    [10, [2, 5]],
-    [42, [2, 3, 7]],
-    [70, [2, 5, 7]],
-    [74, [2, 37]],
-    [92, [2, 2, 23]],
-  ])(`should take a number and return an array of its prime factorials`, (a, expected) => {
-
-    expect(findPrimeFactorials(a)).toStrictEqual(expected);
-  });
+describe("evaluate and return the boolean expression from a string input", () => {
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "TRUE";
+      expect(booleanCalculator(input)).toEqual(true)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "FALSE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "NOT TRUE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "TRUE AND FALSE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "TRUE AND TRUE";
+      expect(booleanCalculator(input)).toEqual(true)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "TRUE OR FALSE";
+      expect(booleanCalculator(input)).toEqual(true)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "FALSE OR FALSE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "FALSE OR FALSE OR FALSE AND FALSE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "FALSE AND FALSE OR FALSE OR TRUE";
+      expect(booleanCalculator(input)).toEqual(true)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "TRUE OR FALSE AND NOT FALSE";
+      expect(booleanCalculator(input)).toEqual(true)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "(TRUE OR TRUE OR TRUE) AND FALSE";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
+  it("returns the correct boolean expression for single value input",
+    () => {
+      const input:string = "NOT (TRUE AND TRUE)";
+      expect(booleanCalculator(input)).toEqual(false)
+    });
 });
